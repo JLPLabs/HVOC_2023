c7cd15f539e11798d079016b6b6d4d5c88fe53e0
JLPLabs <4744024+JLPLabs@users.noreply.github.com>
2023-02-06 09:23:23 -0600

    fixed lab3 gw

--- lab3/bench/gw.py
+++ lab3/bench/gw.py
@@ -7,6 +7,7 @@ This example demonstrates how to use async IO with python-can.
 
 This is a bi-directional gateway.
 """
+import math
 import sys, getopt
 
 import asyncio
@@ -18,20 +19,26 @@ from can.notifier import MessageRecipient
 #------------------------------------------------------------------------------
 # globals
 bustype = 'socketcan'
-bus0 = ''
-bus1 = ''
+bus0 = ''              # bus0 connection (for port0)
+bus1 = ''              # bus1 connection (for port1)
+f = ''                 # file handle for log file
 
 # globals set by command line arguments (w/ default, if any)
 port0 = 'vcan0'
 port1 = 'vcan1'
+filename = ''
 verbose = False
+dolog = False        # true when we should write to file
 
 usage = """
 -0  --port0  <bus_name>    set bus connected to port 0
 -1  --port1  <bus_name>    set bus connected to port 1
+-o  --out    <file_name>   save a copy of the forwarded data in a log file
 -v                         verbose output to show port values being used
 
-default: -0 {0} -1 {1}
+if -o / --out is not specified then no file is created.
+
+default: --port0 {0} --port1 {1} 
 """.format(port0, port1)
 
 #------------------------------------------------------------------------------
@@ -41,27 +48,77 @@ import signal
 
 # signal handler
 def handler(signum, frame):
+  global dolog, f
+  if dolog:
+    f.close()
   exit(1)
 signal.signal(signal.SIGINT, handler)
+
+# convert speed to j1939 2 bytes
+# usage: byte1, bytes2 = speed2j1939(20)
+#        data = [byte1, byte2, ...remaining 6 bytes...]
+def speed2j1939(mph):
+    # convert mph to meter/sec
+    mps = mph / 2.23694
+ 
+    # convert m/s to bytes
+    byte2 = math.floor(mps/0.256)
+    byte1 = math.floor((mps-byte2*0.256)/0.001)
+  
+    byte2 = max(byte2, 255)
+    byte1 = max(byte2, 255)
+    return byte1, byte2
+
+# convert FE49 to mph   [ground speed]
+def pgnFE49(data):
+    b1 = data[0]
+    b2 = data[1]
+    mps = b2 * 0.256 + b1 * 0.001
+    mph = mps * 2.23694
+    return b1, b2, mph
+  
+def doublespeed(msg: can.Message) -> can.Message:
+    canid = msg.arbitration_id
+    pgn = (canid >> 8) & 0xffff
+    if pgn == 0xFE49: 
+       b1, b2, speed = pgnFE49(msg.data)
+       speed = speed * 2
+       b1, b2  = speed2j1939(speed)
+       msg.data[0] = b1
+       msg.data[1] = b2
+       return msg     # return msg with doubled ground speed
+    else:
+       return msg     # msg wasn't ground speed, so return it unchanged
 
 def print_message(msg: can.Message) -> None:
     print(msg)
 
+def write_message(msg: can.Message) -> None:
+    global dolog, f
+    if dolog:
+      f.write(msg.__str__())
+      f.write("\n")
+
 def fwd_0to1(msg: can.Message) -> None:
     global bus1
+    #msg = doublespeed(msg)
     bus1.send(msg)
+    write_message(msg)
 
 def fwd_1to0(msg: can.Message) -> None:
     global bus0
+    #msg = doublespeed(msg)
     bus0.send(msg)
+    write_message(msg)
 
 
 # handle command line args
 def set_globals(argv):
-  global channel, verbose
+  global port0, port1, verbose, filename, dolog
   try:
-    opts, args = getopt.getopt(argv, "hv0:1:",["port0=","port1="])
-  except getopt.GetoptError:
+    opts, args = getopt.getopt(argv, "hv0:1:o:",["port0=","port1=","out="])
+  except getopt.GetoptError as err:
+    print(err)
     print(usage)
     sys.exit(2)
   for opt, arg in opts:
@@ -74,18 +131,27 @@ def set_globals(argv):
       port0 = arg
     elif opt in ("-1", "--port1"):
       port1 = arg
+    elif opt in ("-o", "--out"):
+      dolog = True
+      filename = arg
 
   if verbose:
-    print("bus: %s" % (channel))
+    print("   port0: %s" % (port0))
+    print("   port1: %s" % (port1))
+    print("filename: %s" % (filename))
+    print(" logging: %s" % (dolog))
 
 
 # =============================================================================
 async def main() -> None:
     """The main function that runs in the loop."""
-    global bus0, bus1
+    global bus0, bus1, dolog, f
 
-    bus0 = can.Bus(interface="socketcan", channel=port0) 
-    bus1 = can.Bus(interface="socketcan", channel=port1)
+    bus0 = can.Bus(interface="socketcan", channel=port0, fd=True) 
+    bus1 = can.Bus(interface="socketcan", channel=port1, fd=True)
+
+    if dolog:
+      f = open(filename, "w")
 
     # port1 listeners ..............................................
     reader1 = can.AsyncBufferedReader()
@@ -114,7 +180,7 @@ async def main() -> None:
     loop0 = asyncio.get_running_loop()
     notifier0 = can.Notifier(bus0, listeners0, loop=loop0)
 
-    print("forwarding between %s to %s" %(port0, port1))
+    print("forwarding between %s and %s" %(port0, port1))
     print(" ")
     print("use 'cansniffer' on either or both vcan networks to watch traffic")
     print("..........")
