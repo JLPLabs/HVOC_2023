e958256f6f44d61c19ee6dd683fe3c07a1cf5555
JLPLabs <4744024+JLPLabs@users.noreply.github.com>
2023-02-22 09:24:32 -0600

    Update randbits.py

--- lab6/classroom/randbits.py
+++ lab6/classroom/randbits.py
@@ -2,3 +2,7 @@ import secrets
 
 x = secrets.randbits(8)
 print(x)
+
+x = secrets.randbits(64)
+xb = x.to_bytes(8, "big")        # converts 64 bits into 8 bytes
+print(xb.hex(" ", 4))            # example: 61e015f7 920a8f0a
