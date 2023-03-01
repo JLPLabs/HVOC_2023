3ee00b4c01d4e04772f2f970d90587ec9258f6aa
JLPLabs <4744024+JLPLabs@users.noreply.github.com>
2023-03-01 10:16:52 -0600

    filled in missing details around KDF

--- lab6/classroom/randbits_and_kdf.py
+++ lab6/classroom/randbits_and_kdf.py
@@ -2,6 +2,8 @@ from cryptography.hazmat.primitives import hashes                        # for S
 from cryptography.hazmat.primitives.kdf.concatkdf import ConcatKDFHash   # for kdf
 import secrets                                                           # for randbits
 
+# x and y will be entropy ('nonces' in this case)
+
 x = secrets.randbits(8)
 print(x)
 
@@ -12,13 +14,19 @@ print(xb.hex(" ", 4))            # example: 61e015f7 920a8f0a
 y = secrets.randbits(64)
 yb = y.to_bytes(8, "big")        # example: 13a56ccd bed1a7c8
 
+# z is the conatenation of all nonces
 zb = xb + yb
 print(zb.hex(" ", 4))            # example: 61e015f7 920a8f0a 13a56ccd bed1a7c8
 
+# Sn is the long-lived base key
 Sn = bytes.fromhex("00000000 11111111 22222222 33333333")
 
+# combine base with nonces (order matters! all participants mush use the same order)
 keymaterial = Sn + zb
 print(keymaterial.hex(" ", 4))   # example: 00000000 11111111 22222222 33333333 61e015f7 920a8f0a 13a56ccd bed1a7c8
 
-sv = kdf.derive(keymaterial)
-print(sv.hex(" ", 4))            # example: fb2e494f df1c65a1 01e8649f f59f7bd6
+# use the key material to create a new key, Sv
+kdf = ConcatKDFHash(algorithm=hashes.SHA256(), length=16, otherinfo=b"00000000")
+Sv = kdf.derive(keymaterial)
+
+print(Sv.hex(" ", 4))            # example: fb2e494f df1c65a1 01e8649f f59f7bd6
